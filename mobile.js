4ce1759f5f1c9b8856bc0abc2ceb759781178208
goldcvt <thegoldcat5@gmail.com>
2021-10-07 21:48:15 +0300

    rework slideshow logic, add forced coloring of progress bar sections

--- mobile.js
+++ mobile.js
@@ -1,41 +1,98 @@
+const DEFAULT_ANIMATION_DURATION = 6000
+
 const mainSlide = document.querySelector('.main-slide')
 const progressBar = document.querySelector('.progress-bar')
 const pBarSections = progressBar.querySelectorAll('.progress-section')
+const pBarProgressMasks = progressBar.querySelectorAll('.progress-section_playmask')
 const numberOfSlides = mainSlide.childElementCount
 const slides = document.querySelectorAll('.slide')
 const screenWidth = document.querySelector('.container').clientWidth
 let currentSlideIndex = 0
+let animationFrameId = undefined
+let interval = undefined
 
 const moveSlides = (direction) => {
+    // TODO clear all animations
+    // if (animationFrameId) {
+    //     cancelAnimationFrame(animationFrameId)
+    // }
+    // // TODO clear interval
+    // console.log(interval)
+    // if (interval) clearTimeout(interval)
+    // console.log(interval)
+    
     if (direction === 'left') {
-        if (currentSlideIndex === 0) {
-            currentSlideIndex = (numberOfSlides - 1)
-        } else {
+        if (currentSlideIndex !== 0) {
             currentSlideIndex--
         }
     } else if (direction === 'right') {
-        if (currentSlideIndex >= numberOfSlides - 1) {
-            currentSlideIndex = 0
-        } else {
+        if (currentSlideIndex < numberOfSlides - 1) {
             currentSlideIndex++
         }
     }
-    mainSlide.style.transform = `translateX(-${currentSlideIndex * screenWidth}px)`
+    
+    mainSlide.style.transform = `translateX(-${currentSlideIndex * screenWidth + 0.5}px)`
+    assureRestProgressSections(currentSlideIndex)
+    console.log(currentSlideIndex)
+    // TODO set new animateCurrent
+    // animateCurrent()
+    
+}
+
+const animateCurrent = () => {
+    let element = pBarSections[currentSlideIndex].children[0]
+
+    let animationDuration = DEFAULT_ANIMATION_DURATION
+    
+    if (slides[currentSlideIndex].classList.contains('video')) {
+        animationDuration = parseTime(slides[currentSlideIndex].getAttribute('video-duration')) * 1000
+    }
+    
+    
+    // animate({
+    //     duration: animationDuration,
+    //     draw: bleach,
+    //     timing: (timeFraction) => timeFraction,
+    //     element: element
+    // })
+    
+
+    // Timing trigger
+    if (currentSlideIndex < numberOfSlides - 1) {
+        interval = setTimeout(() => {
+            moveSlides('right')
+            animateCurrent()
+        }, animationDuration)
+    }
+}
+
+// LOGIC
+// Если я на индексе Х, то этот индекс закрашивать понемногу (анимация только тут), предыдущие целиком мгновенно, 
+// а следующие наоборот в черный
+
+const darken = (progress, element) => {
+    element.style.width = 0 + '%'
 }
 
 const bleach = (progress, element) => {
     element.style.width = progress * 100 + '%'
+}
 
+const assureRestProgressSections = (currentIndex) => {
+    pBarProgressMasks.forEach((element, index) => {
+        if (index < currentIndex) {
+            bleach(1, element)
+        // current index getting darkened too bc we want to clear it when moving to the left
+        } else {
+            darken(1, element)
+        }
+    })
 }
 
 const animate = ({duration, draw, timing, element}) => {
+    let myHandle;
     let start = performance.now();
-    const myHandle = requestAnimationFrame(function animate(time) {
-        if (element != pBarSections[currentSlideIndex]) {
-            cancelAnimationFrame(myHandle)
-            // almost does the thing, but not quite
-            // TODO fix behaviour with skips
-        }
+    function animate(time) {
         let timeFraction = (time - start) / duration;
         if (timeFraction > 1) timeFraction = 1;
 
@@ -43,39 +100,13 @@ const animate = ({duration, draw, timing, element}) => {
 
         draw(progress, element);
 
+        // this thing's unstoppable...
         if (timeFraction < 1) {
-            requestAnimationFrame(animate);
+            myHandle = requestAnimationFrame(animate);
         }
-
-        // if (timeFraction = 1) {
-        //     nextSlide()
-        // }
-    });
-}
-
-const nextSlide = () => {
-    let timeout = 6000
-    let currentSection = pBarSections[currentSlideIndex]
-
-    // TODO sync a video duration to timeout. Probably via custom prop "video-length" in a slide element
-    if (slides[currentSlideIndex].classList.contains('video')) {
-        timeout = 1000
     }
-
-    animate({
-        duration: timeout,
-        draw: bleach,
-        timing: (timeFraction) => timeFraction,
-        element: currentSection.firstChild
-    })
-
-    if (currentSlideIndex < numberOfSlides - 1) {
-
-        setTimeout(() => {
-            moveSlides('right')
-            nextSlide()
-        }, timeout + 300)
-    }
+    myHandle = requestAnimationFrame(animate)
+    animationFrameId = myHandle
 }
 
 // Kinda sus, we're on mobile, there's no keys...
@@ -89,6 +120,6 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-// actually, should only wait for load this div's content
+// actually, should wait for load this div's content
 // TODO add such a behaviour
-window.onload = nextSlide
+animateCurrent()
