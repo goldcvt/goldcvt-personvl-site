c0eb6bd12bfc7100917cb46f8661f6ad8fb13049
goldcvt <thegoldcat5@gmail.com>
2021-10-07 14:51:06 +0300

    add mask div for bleaching out progress bar sections

--- mobile.js
+++ mobile.js
@@ -1,6 +1,8 @@
 const mainSlide = document.querySelector('.main-slide')
 const progressBar = document.querySelector('.progress-bar')
+const pBarSections = progressBar.querySelectorAll('.progress-section')
 const numberOfSlides = mainSlide.childElementCount
+const slides = document.querySelectorAll('.slide')
 const screenWidth = document.querySelector('.container').clientWidth
 let currentSlideIndex = 0
 
@@ -21,14 +23,58 @@ const moveSlides = (direction) => {
     mainSlide.style.transform = `translateX(-${currentSlideIndex * screenWidth}px)`
 }
 
+const bleach = (progress, element) => {
+    element.style.width = progress * 100 + '%'
+    
+}
+
+const animate = ({duration, draw, timing, element}) => {
+    let start = performance.now();
+    const myHandle = requestAnimationFrame(function animate(time) {
+        if (element != pBarSections[currentSlideIndex]) {
+            cancelAnimationFrame(myHandle)
+            // almost does the thing, but not quite
+            // TODO fix behaviour with skips
+        }
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        let progress = timing(timeFraction)
+
+        draw(progress, element);
+        
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+        // if (timeFraction = 1) {
+        //     nextSlide()
+        // }
+    });
+}
+
 const nextSlide = () => {
-    if (currentSlideIndex < numberOfSlides - 1) {    
-        moveSlides('right')
-        setTimeout(() => nextSlide(), 6000)
+    let timeout = 6000
+    let currentSection = pBarSections[currentSlideIndex]
+    
+    // TODO sync a video duration to timeout. Probably via custom prop "video-length" in a slide element
+    if (slides[currentSlideIndex].classList.contains('video')) {
+        timeout = 1000
+    }
+
+    animate({duration: timeout, draw: bleach, timing: (timeFraction) => timeFraction, element: currentSection.firstChild})
+
+    if (currentSlideIndex < numberOfSlides - 1) {
+        
+        setTimeout(() => {
+            moveSlides('right')
+            nextSlide()
+        }, timeout + 300)
     }
 }
 
 // Kinda sus, we're on mobile, there's no keys...
+// TODO add section skip handler
 document.addEventListener('keydown', (event) => {
     console.log(event)
     if (event.key === 'ArrowLeft') {
@@ -38,4 +84,6 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-nextSlide()
+// actually, should only wait for load this div's content
+// TODO add such a behaviour
+window.onload = nextSlide
