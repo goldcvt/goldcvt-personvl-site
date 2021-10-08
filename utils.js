7f27867d710b04ed00b6c87a48f76dfe574b6e87
goldcvt <thegoldcat5@gmail.com>
2021-10-08 16:25:47 +0300

    Create stoppable timer class

--- utils.js
+++ utils.js
@@ -3,3 +3,52 @@ const parseTime = (timeString) => {
         return parseInt(prev) * 60 + parseInt(cur)
     })
 }
+
+// Shotouts to Tim Down from SO
+class Timer {
+    constructor(callback, delay) {
+        this.callback = callback
+        this.timerId = undefined
+        this.start = undefined
+        this.remaining = delay;
+
+        this.resume();
+    }
+
+    pause() {
+        //clearTimeout(this.timerId)
+        this.clear()
+        
+        this.remaining -= Date.now() - this.start;
+    }
+
+    resume() {
+        //console.log("Will resume timers:", Timer.timers)
+        this.start = Date.now();
+
+        // clearTimeout(this.timerId);
+        this.clear()
+
+        this.timerId = setTimeout(this.callback, this.remaining);
+        Timer.timers.push(this.timerId)
+
+        //console.log("Resumed, timers:", Timer.timers)
+    }
+
+    clearAll() {
+        // console.log("Will clear all:", Timer.timers)
+        Timer.timers.forEach(timerId => {
+            clearTimeout(timerId)
+        })
+        Timer.timers = []
+        // console.log("Cleared all", Timer.timers)
+    }
+
+    clear() {
+        // console.log(`Will clear ${this.timerId}`, Timer.timers)
+        clearTimeout(this.timerId)
+        Timer.timers = Timer.timers.filter(id => id !== this.timerId)
+        // console.log("Cleared one", Timer.timers)
+    }
+}
+Timer.timers = []
