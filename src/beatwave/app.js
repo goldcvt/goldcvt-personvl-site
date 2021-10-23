01fe8f1ca21394ecfe18bbd79e6fadf3a6277f2f
goldcvt <thegoldcat5@gmail.com>
2021-10-23 23:13:59 +0300

    Add musical part to beatwave app

--- src/beatwave/app.js
+++ src/beatwave/app.js
@@ -1,15 +1,54 @@
+import * as Tone from 'tone'
+
+let synth
+
 const board = document.querySelector('#board')
+const startButton = document.querySelector('.modal button')
+const modal = document.querySelector('.container-modal')
+const clearFieldButton = document.querySelector('.clear-field')
+const muteButton = document.querySelector('.mute')
+
+const volume = new Tone.Volume(-12).toDestination()
 
 const DELAYBETWEENCOLUMNS = 100
-const TILES_COUNT = 40*20
+const TILES_COUNT = 40 * 20
 const COLORS = ['#8e44ad', '#D39B23', '#3498db', 'teal', '#2ecc71']
+const NOTES = [
+    'Ab3',
+    'A#3',
+    'C3',
+    'Eb3',
+    'F3',
+    'Ab4',
+    'A#4',
+    'C4',
+    'Eb4',
+    'F4',
+    'Ab5',
+    'A#5',
+    'C5',
+    'Eb5',
+    'F5',
+    'Ab6',
+    'A#6',
+    'C6',
+    'Eb6',
+    'F6',
+]
 
 for (let i = 0; i < TILES_COUNT; i++) {
     const tile = document.createElement('div')
     tile.classList.add('tile')
-    tile.state = {plays: false, on: false}
+
+    tile.state = { plays: false, on: false, note: NOTES[NOTES.length - Math.floor(i / 40) - 1] }
     tile.addEventListener('click', () => toggleColor(tile))
     board.append(tile)
+}
+
+const startApp = () => {
+    synth = new Tone.PolySynth(Tone.Synth).connect(volume)
+    Tone.start()
+    modal.classList.add('hide')
 }
 
 function toggleColor(tile) {
@@ -30,7 +69,7 @@ function resetColor(tile) {
 
 function resetGlow(tile) {
     tile.style.boxShadow = '0 0 2px #000'
-} 
+}
 
 function getRandomColor() {
     const index = Math.ceil(Math.random() * COLORS.length)
@@ -42,27 +81,37 @@ function glow(tile) {
 }
 
 function getActiveTiles(markerPosition) {
-    let tilesToPlay = [];
+    let tilesToPlay = []
     board.querySelectorAll('.tile').forEach((node, index) => {
-        if (index % 40 === markerPosition) tilesToPlay.push(node);
+        if (index % 40 === markerPosition) tilesToPlay.push(node)
     })
-    return tilesToPlay;
+    return tilesToPlay
 }
 
 function playbackLoop(playbackMarkerPosition) {
     let newPlaybackMarkerPosition = movePlaybackMarker(playbackMarkerPosition)
     return setTimeout(() => {
-            playbackLoop(newPlaybackMarkerPosition)
-        }, DELAYBETWEENCOLUMNS)
-    
+        playbackLoop(newPlaybackMarkerPosition)
+    }, DELAYBETWEENCOLUMNS)
+}
+
+function getNotes(tiles) {
+    return tiles
+        .map((tile) => {
+            if (tile.state.plays && tile.state.on) {
+                return tile.state.note
+            }
+        })
+        .filter((val) => val !== undefined)
 }
 
 function playSound(tiles) {
-    
+    const notes = getNotes(tiles)
+    notes.length !== 0 ? synth.triggerAttackRelease(notes, 1) : null
 }
 
 function displayMarker(tiles) {
-    tiles.forEach(tile => {
+    tiles.forEach((tile) => {
         if (tile.state.plays) {
             //!tile.state.on ? tile.style.backgroundColor = 'white' : null
             if (!tile.state.on) {
@@ -77,7 +126,7 @@ function displayMarker(tiles) {
 }
 
 function movePlaybackMarker(markerPosition) {
-    let prevMarkerPos;
+    let prevMarkerPos
     if (markerPosition - 1 < 0) {
         prevMarkerPos = 39
     } else {
@@ -85,7 +134,7 @@ function movePlaybackMarker(markerPosition) {
     }
     let tilesToUnplay = getActiveTiles(prevMarkerPos)
     tilesToUnplay.forEach((tile) => {
-            tile.state.plays = false 
+        tile.state.plays = false
     })
     // display changes
     displayMarker(tilesToUnplay)
@@ -100,19 +149,28 @@ function movePlaybackMarker(markerPosition) {
     playSound(tilesToPlay)
 
     if (markerPosition === 39) {
-        return 0;
+        return 0
     } else {
-        return markerPosition + 1;
+        return markerPosition + 1
     }
 }
 
-function pulseOnPlay() {
-
-}
-
+function pulseOnPlay() {}
 
 playbackLoop(0)
 
-alert(`Активируйте ячейки, чтобы белый макер извлекал из них звуки! Тыкайте мышкой на квадратики, чтобы поместить ноту. Звука пока нет)
-Растяните окно пошире, чтобы маркер выглядел по-человечески
-`)
+startButton.addEventListener('click', startApp)
+clearFieldButton.addEventListener('click', (e) => {
+    document.querySelectorAll('.tile').forEach((tile) => {
+        tile.state.on ? toggleColor(tile) : null
+    })
+})
+muteButton.addEventListener('click', (e) => {
+    if (muteButton.textContent === 'Выключить звук') {
+        volume.mute = true
+        muteButton.textContent = 'Включить звук'
+    } else {
+        volume.mute = false
+        muteButton.textContent = 'Выключить звук'
+    }
+})
